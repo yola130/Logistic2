d3f637cb6ace2750726ee698db2c236dd08adf5c
yola130 <1305451838@qq.com>
2024-12-21 20:01:24 +0800

    Update logistic.py

--- logistic.py
+++ logistic.py
@@ -2,8 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 import pandas as pd
-import shap
-import matplotlib.pyplot as plt
+
 
 # Load the model
 model = joblib.load('Logistic.pkl')
