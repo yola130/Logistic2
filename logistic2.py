1bf2b6911338564fb9174dfb0f30d03f989e943e
yola130 <1305451838@qq.com>
2024-12-21 21:23:51 +0800

    Update logistic2.py

--- logistic2.py
+++ logistic2.py
@@ -13,34 +13,34 @@ feature_names = ["ALB", "LDH", "HCO3", "ALP", "NLR", "Fg", "PT", "Neutrophils", 
 st.title("COVID-19 Subphenotype Classifier")
 
 # ALB: numerical input
-ALB = st.number_input("ALB:", min_value=0, max_value=100, value=35)
+ALB = st.number_input("ALB:", min_value=0.0, max_value=100.0, value=35.0, step=0.01, format="%0.2f")
 
 # LDH: numerical input
-LDH = st.number_input("LDH:", min_value=50, max_value=4000, value=270)
+LDH = st.number_input("LDH:", min_value=50.0, max_value=4000.0, value=270.0, step=0.01, format="%0.2f")
 
 # ALP: numerical input
-ALP = st.number_input("ALP:", min_value=0, max_value=2000, value=80)
+ALP = st.number_input("ALP:", min_value=0.0, max_value=2000.0, value=80.0, step=0.01, format="%0.2f")
 
 # Neutrophils: numerical input
-Neutrophils = st.number_input("Neutrophils:", min_value=0, max_value=50, value=6)
+Neutrophils = st.number_input("Neutrophils:", min_value=0.0, max_value=50.0, value=6.0, step=0.01, format="%0.2f")
 
 # Fg: numerical input
-Fg = st.number_input("Fg:", min_value=0, max_value=50, value=3)
+Fg = st.number_input("Fg:", min_value=0.0, max_value=50.0, value=3.0, step=0.01, format="%0.2f")
 
 # DBIL: numerical input
-DBIL = st.number_input("DBIL:", min_value=0, max_value=100, value=5)
+DBIL = st.number_input("DBIL:", min_value=0.0, max_value=100.0, value=5.0, step=0.01, format="%0.2f")
 
 # NLR: numerical input
-NLR = st.number_input("NLR:", min_value=0, max_value=500, value=5)
+NLR = st.number_input("NLR:", min_value=0.0, max_value=500.0, value=5.0, step=0.01, format="%0.2f")
 
 # BUN: numerical input
-BUN = st.number_input("BUN:", min_value=0, max_value=200, value=5)
+BUN = st.number_input("BUN:", min_value=0.0, max_value=200.0, value=5.0, step=0.01, format="%0.2f")
 
 # HCO3: numerical input
-HCO3 = st.number_input("HCO3:", min_value=0, max_value=100, value=25)
+HCO3 = st.number_input("HCO3:", min_value=0.0, max_value=100.0, value=25.0, step=0.01, format="%0.2f")
 
 # PT: numerical input
-PT = st.number_input("PT:", min_value=0, max_value=100, value=12)
+PT = st.number_input("PT:", min_value=0.0, max_value=100.0, value=12.0, step=0.01, format="%0.2f")
 
 # Process inputs and make predictions
 # feature_values = [ALB, LDH, HCO3, ALP, NLR, Fg, PT, Neutrophils, DBIL, BUN]
@@ -52,7 +52,6 @@ features = pd.DataFrame(data)
 if st.button("Predict"):
     # Predict probabilities
     predicted_proba = model.predict_proba(features)[0]
-    st.text(predicted_proba)
     
     # 根据预测概率的最高值来确定预测类别（但这里我们直接根据概率阈值判断）  
     high_risk_threshold = 0.36  # 36% 的阈值  
